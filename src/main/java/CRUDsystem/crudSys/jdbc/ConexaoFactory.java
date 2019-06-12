42cac18fcb84ecbe64393689f2226d3576df74ee
fguilhermepll <51724521+fguilhermepll@users.noreply.github.com>
2019-06-11 21:31:47 -0300

    Update ConexaoFactory.java

--- src/main/java/CRUDsystem/crudSys/jdbc/ConexaoFactory.java
+++ src/main/java/CRUDsystem/crudSys/jdbc/ConexaoFactory.java
@@ -10,7 +10,7 @@ public class ConexaoFactory {
 		
 		try {
 			Class.forName("org.postgresql.Driver");
-			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/crudSys", "postgres", "postgres");
+			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/crudSys", "database-login", "database-pass");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} catch (ClassNotFoundException e) {
